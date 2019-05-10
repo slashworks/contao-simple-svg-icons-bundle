adf09a2179f1c2ccdeaba8639569feb6169b128a
Stefan Gruna <stefang@slash-works.de>
2019-05-10 13:27:52 +0200

    Fixing wrong namespace in Plugin class

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Slashworks\SimpleSvgIconsBundle\ContaoManager;
+namespace Slashworks\ContaoSimpleSvgIconsBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
